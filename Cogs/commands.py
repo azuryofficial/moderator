616061f9e5a345c19f726166340b0cfe98175a6e
citharus <citharus@protonmail.com>
2021-05-15 18:54:21 +0200

    :sparkles: Added kick command

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -1,6 +1,12 @@
+import discord
 from discord.ext import commands
 
 
 class Commands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot: commands.Bot = bot
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx: commands.Context, user: discord.Member, *, reason: str = None) -> None:
+        await user.kick(reason=reason)
