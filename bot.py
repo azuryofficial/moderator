269178e6e786d6baf60ddbafffff9ba59e49b725
citharus <citharus@protonmail.com>
2021-05-15 22:58:57 +0200

    :ambulance: Added forgotten parameter transfer

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ from Cogs import Kick, Ban
 bot: commands.Bot = commands.Bot("m.")
 bot.remove_command("help")
 
-bot.add_cog(Kick)
-bot.add_cog(Ban)
+bot.add_cog(Kick(bot))
+bot.add_cog(Ban(bot))
 
 if __name__ == "__main__":
     bot.run(dotenv_values(".env")["TOKEN"])
