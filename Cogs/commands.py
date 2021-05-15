209f17b2e86cca476800122676078a76629b639f
citharus <citharus@protonmail.com>
2021-05-15 19:21:29 +0200

    :sparkles: Added unban command

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -15,3 +15,13 @@ class Commands(commands.Cog):
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx: commands.Context, user: discord.Member, *, reason: str = None) -> None:
         await user.ban(reason=reason)
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx: commands.Context, *, user: str) -> None:
+        bans: list[discord.Member] = await ctx.guild.bans()
+
+        for ban in bans:
+            if (ban.user.name, ban.user.discriminator) == (*user.split("#"),):
+                await ban.unban()
+                break
