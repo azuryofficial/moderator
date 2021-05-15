f6419c63b7386028c8cfaa1c43f9d4a304f1cf76
citharus <citharus@protonmail.com>
2021-05-15 22:46:42 +0200

    :truck: Moved ban commands

--- Cogs/commands.py
+++ Cogs/commands.py
@@ -10,18 +10,3 @@ class Commands(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
         await member.kick(reason=reason)
-
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
-        await member.ban(reason=reason)
-
-    @commands.command()
-    @commands.has_permissions(ban_members=True)
-    async def unban(self, ctx: commands.Context, *, member: str) -> None:
-        banned_user: list[discord.Member] = await ctx.guild.bans()
-
-        for user in banned_user:
-            if (user.user.name, user.user.discriminator) == (*member.split("#"),):
-                await user.unban()
-                break
