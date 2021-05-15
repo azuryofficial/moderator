c4c21fbe048aca8ee5566a4f401e6620b6e608e3
citharus <citharus@protonmail.com>
2021-05-15 17:51:09 +0200

    ✨ Added Commands to bot

--- bot.py
+++ bot.py
@@ -1,8 +1,12 @@
 from discord.ext import commands
 from dotenv import dotenv_values
 
+from Cogs import Commands
+
 bot: commands.Bot = commands.Bot("m.")
 bot.remove_command("help")
+
+bot.add_cog(Commands(bot))
 
 if __name__ == "__main__":
     bot.run(dotenv_values(".env")["TOKEN"])
