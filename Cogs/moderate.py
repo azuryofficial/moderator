f9ccc6e374985d9ad0e36dbc2f21fc7121f22e99
citharus <citharus@protonmail.com>
2021-05-16 00:32:42 +0200

    :sparkles: Added embed to kick command

--- Cogs/moderate.py
+++ Cogs/moderate.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+import embeds
+
 
 class Kick(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -10,6 +12,7 @@ class Kick(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
         await member.kick(reason=reason)
+        await ctx.send(embed=discord.Embed().from_dict(embeds.KickEmbed(member).embed))
 
 
 class Ban(commands.Cog):
