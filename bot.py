d6dd24be38c2e6e5afeb1a581035ae6af544779f
citharus <citharus@protonmail.com>
2021-05-15 22:55:30 +0200

    :truck: Changed Cogs import

--- bot.py
+++ bot.py
@@ -1,12 +1,13 @@
 from discord.ext import commands
 from dotenv import dotenv_values
 
-from Cogs import Commands
+from Cogs import Kick, Ban
 
 bot: commands.Bot = commands.Bot("m.")
 bot.remove_command("help")
 
-bot.add_cog(Commands(bot))
+bot.add_cog(Kick)
+bot.add_cog(Ban)
 
 if __name__ == "__main__":
     bot.run(dotenv_values(".env")["TOKEN"])
