37468733bea58997cdbd48945fec459057704eba
citharus <citharus@protonmail.com>
2021-05-16 18:17:29 +0200

    :sparkles: Added Mute Cog

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 from discord.ext import commands
 from dotenv import dotenv_values
 
-from Cogs import Kick, Ban
+from Cogs import Kick, Ban, Mute
 
 bot: commands.Bot = commands.Bot("m.")
 bot.remove_command("help")
 
 bot.add_cog(Kick(bot))
 bot.add_cog(Ban(bot))
+bot.add_cog(Mute(bot))
 
 if __name__ == "__main__":
     bot.run(dotenv_values(".env")["TOKEN"])
