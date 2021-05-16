23ed18588edfa9b5e031f6ff61e4f8446a96edd7
citharus <citharus@protonmail.com>
2021-05-16 17:28:40 +0200

    :sparkles: Added Embed base class

--- embeds.py
+++ embeds.py
@@ -1,6 +1,11 @@
 import discord
 
 
+class Embed:
+    def __init__(self, color: int, description: str) -> None:
+        self.embed = {"color:": color, "description": description}
+
+
 class KickEmbed:
     def __init__(self, member: discord.Member) -> None:
         self.embed = {"color": 15105570, "description": f"Kicked {member.mention}"}
