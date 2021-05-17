8ac72361f2a0dbf93ba8d730e443188321415af3
citharus <citharus@protonmail.com>
2021-05-17 20:53:41 +0200

    :sparkles: Added CommandNotFound error handling

--- Cogs/error.py
+++ Cogs/error.py
@@ -7,7 +7,10 @@ from embeds import ErrorEmbed
 class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error) -> None:
-        if isinstance(error, commands.MissingRequiredArgument):
+        if isinstance(error, commands.CommandNotFound):
+            await ctx.send(embed=ErrorEmbed("This command does not exist."))
+
+        elif isinstance(error, commands.MissingRequiredArgument):
             if "member" in error.args[0]:
                 await ctx.send(embed=ErrorEmbed("You have to specify a member."))
 
