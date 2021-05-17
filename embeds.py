220654275422fcc8ba6da71e73f3c6f0a40bbd80
citharus <citharus@protonmail.com>
2021-05-17 18:39:01 +0200

    :sparkles: Added ban command embed

--- embeds.py
+++ embeds.py
@@ -9,3 +9,8 @@ class Embed(discord.Embed):
 class KickEmbed(Embed):
     def __init__(self, member: discord.Member):
         super().__init__("", f"Kicked {member.mention}", discord.Color.orange())
+
+
+class BanEmbed(Embed):
+    def __init__(self, member: discord.Member):
+        super().__init__("", f"Banned {member.mention}", discord.Color.orange())
