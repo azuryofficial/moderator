79016ae1c10b112018bc935ec70e2d6b1a547f4a
citharus <citharus@protonmail.com>
2021-05-17 20:44:45 +0200

    :truck: Moved Ban Cog

--- Cogs/__init__.py
+++ Cogs/__init__.py
@@ -1,3 +1,4 @@
+from Cogs.ban import Ban
 from Cogs.error import Error
 from Cogs.kick import Kick
-from Cogs.moderate import Ban, Mute
+from Cogs.moderate import Mute
---
+++ Cogs/ban.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+
+from embeds import CommandEmbed
+
+
+class Ban(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot: commands.Bot = bot
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
+        await member.ban(reason=reason)
+        await ctx.send(embed=CommandEmbed("Banned", member))
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def unban(self, ctx: commands.Context, *, member: str) -> None:
+        banned_user: list[discord.Member] = await ctx.guild.bans()
+
+        for user in banned_user:
+            if (user.user.name, user.user.discriminator) == (*member.split("#"),):
+                await user.unban()
+                await ctx.send(embed=CommandEmbed("Unbanned", user))
+                break
