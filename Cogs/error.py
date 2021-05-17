4cdaebf22877908cd92dfb1100fc6cec9e34d1e7
citharus <citharus@protonmail.com>
2021-05-17 18:57:31 +0200

    :sparkles: Added RoleNotFound error handling

--- Cogs/error.py
+++ Cogs/error.py
@@ -1,7 +1,12 @@
+import discord
 from discord.ext import commands
 
 
 class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error) -> None:
-        pass
+        if isinstance(error, commands.RoleNotFound):
+            if "Muted" in error:
+                ctx.guild.create_role(name="Muted",
+                                      permissions=discord.Permissions(read_message_history=True, read_messages=True),
+                                      reason="Automatically created role for mute command.")
