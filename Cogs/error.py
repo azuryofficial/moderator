eea82b27e59351d6dcbf0f795a367dcb580c7894
citharus <citharus@protonmail.com>
2021-05-17 20:35:03 +0200

    :ambulance: Fixed if statement condition

--- Cogs/error.py
+++ Cogs/error.py
@@ -6,7 +6,7 @@ class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error) -> None:
         if isinstance(error, commands.RoleNotFound):
-            if "Muted" in error:
+            if "Muted" in error.args[0]:
                 await ctx.guild.create_role(name="Muted",
                                             permissions=discord.Permissions(read_message_history=True,
                                                                             read_messages=True),
