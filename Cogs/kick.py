cbe96eb91b6b63459753ae6c42b9b772a99869b5
citharus <citharus@protonmail.com>
2021-05-17 20:43:21 +0200

    :truck: Moved Kick Cog

---
+++ Cogs/kick.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+from embeds import CommandEmbed
+
+
+class Kick(commands.Cog):
+    def __init__(self, bot: commands.Bot) -> None:
+        self.bot: commands.Bot = bot
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
+        await member.kick(reason=reason)
+        await ctx.send(embed=CommandEmbed("Kicked", member))
