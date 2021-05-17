4aad0eb8bf4b433c8cb3a47f97085846671f4450
citharus <citharus@protonmail.com>
2021-05-17 20:40:43 +0200

    :sparkles: Added Error Cog to Bot

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 from discord.ext import commands
 from dotenv import dotenv_values
 
-from Cogs import Kick, Ban, Mute
+from Cogs import Kick, Ban, Mute, Error
 
 bot: commands.Bot = commands.Bot("m.")
 bot.remove_command("help")
@@ -9,6 +9,7 @@ bot.remove_command("help")
 bot.add_cog(Kick(bot))
 bot.add_cog(Ban(bot))
 bot.add_cog(Mute(bot))
+bot.add_cog(Error())
 
 if __name__ == "__main__":
     bot.run(dotenv_values(".env")["TOKEN"])
