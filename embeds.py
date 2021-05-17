6069e527c5e31d916c723775d16441736ac9aa57
citharus <citharus@protonmail.com>
2021-05-17 19:02:14 +0200

    :sparkles: Added general command embed

--- embeds.py
+++ embeds.py
@@ -6,21 +6,6 @@ class Embed(discord.Embed):
         super().__init__(title=title, description=description, color=color)
 
 
-class KickEmbed(Embed):
-    def __init__(self, member: discord.Member) -> None:
-        super().__init__("", f"Kicked {member.mention}", discord.Color.orange())
-
-
-class BanEmbed(Embed):
-    def __init__(self, member: discord.Member) -> None:
-        super().__init__("", f"Banned {member.mention}", discord.Color.orange())
-
-
-class UnbanEmbed(Embed):
-    def __init__(self, member: discord.Member) -> None:
-        super().__init__("", f"Unbanned {member.mention}", discord.Color.orange())
-
-
-class MuteEmbed(Embed):
-    def __init__(self, member: discord.Member) -> None:
-        super().__init__("", f"Muted {member.mention}", discord.Color.orange())
+class CommandEmbed(Embed):
+    def __init__(self, method: str, member: discord.Member) -> None:
+        super().__init__("", f"{method} {member.mention}", discord.Color.orange())
