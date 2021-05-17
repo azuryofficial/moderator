ea278b13d9124a8890763ab302cf5f70340825e7
citharus <citharus@protonmail.com>
2021-05-17 18:48:45 +0200

    :recycle: Changed mute command embed

--- Cogs/moderate.py
+++ Cogs/moderate.py
@@ -3,7 +3,7 @@ import asyncio
 import discord
 from discord.ext import commands
 
-from embeds import KickEmbed, BanEmbed, UnbanEmbed
+from embeds import KickEmbed, BanEmbed, UnbanEmbed, MuteEmbed
 
 
 class Kick(commands.Cog):
@@ -57,7 +57,7 @@ class Mute(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def mute(self, ctx: commands.Context, member: discord.Member, delay: int = 1, *, reason: str = None) -> None:
         role: discord.Role = await commands.RoleConverter().convert(ctx, "Muted")
-        await ctx.send(embed=discord.Embed(color=discord.Color.orange(), description=f"Muted {member.mention}"))
+        await ctx.send(embed=MuteEmbed(member))
         await member.add_roles(role, reason=reason)
         await asyncio.sleep(delay * 60)
         await member.remove_roles(role)
