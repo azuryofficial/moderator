2d7e14f3131e64ab3372352f850a3a03893c708d
citharus <citharus@protonmail.com>
2021-05-17 20:51:06 +0200

    :sparkles: Added missing member error handling

--- Cogs/error.py
+++ Cogs/error.py
@@ -7,7 +7,11 @@ from embeds import ErrorEmbed
 class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error) -> None:
-        if isinstance(error, commands.RoleNotFound):
+        if isinstance(error, commands.MissingRequiredArgument):
+            if "member" in error.args[0]:
+                await ctx.send(embed=ErrorEmbed("You have to specify a member."))
+
+        elif isinstance(error, commands.RoleNotFound):
             await ctx.send(embed=ErrorEmbed("Missing role was automatically created. Try again."))
             if "Muted" in error.args[0]:
                 await ctx.guild.create_role(name="Muted",
