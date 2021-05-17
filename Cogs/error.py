ed117dbc1ae61278266ff9f0243b5b370c6974cc
citharus <citharus@protonmail.com>
2021-05-17 20:36:02 +0200

    :sparkles: Added RoleNotFound embed

--- Cogs/error.py
+++ Cogs/error.py
@@ -1,11 +1,14 @@
 import discord
 from discord.ext import commands
 
+from embeds import ErrorEmbed
+
 
 class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error) -> None:
         if isinstance(error, commands.RoleNotFound):
+            await ctx.send(embed=ErrorEmbed("Missing role was automatically created. Try again."))
             if "Muted" in error.args[0]:
                 await ctx.guild.create_role(name="Muted",
                                             permissions=discord.Permissions(read_message_history=True,
