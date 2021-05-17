6eb0168b785171a715560fc3818727f0f226e225
citharus <citharus@protonmail.com>
2021-05-17 18:36:07 +0200

    :sparkles: Added KickEmbed

--- embeds.py
+++ embeds.py
@@ -4,3 +4,8 @@ import discord
 class Embed(discord.Embed):
     def __init__(self, title: str, description: str, color: discord.Color) -> None:
         super().__init__(title=title, description=description, color=color)
+
+
+class KickEmbed(Embed):
+    def __init__(self, member: discord.Member):
+        super().__init__("", f"Kicked {member.mention}", discord.Color.orange())
