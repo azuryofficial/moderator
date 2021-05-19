851cf65cce9435329d8c82bad8d77ec91692ac33
citharus <citharus@protonmail.com>
2021-05-19 21:20:55 +0200

    :recycle: Reworked Embed base class to be independent from discord.Embed

--- embeds.py
+++ embeds.py
@@ -1,9 +1,21 @@
 import discord
 
 
-class Embed(discord.Embed):
-    def __init__(self, title: str, description: str, color: discord.Color) -> None:
-        super().__init__(title=title, description=description, color=color)
+class Embed:
+    def __init__(self, title: str, description: str, color: discord.Color, member: discord.Member = None) -> None:
+        self.embed: dict = {
+            "author": {
+                "name": member.display_name if member else "",
+                "icon_url": str(member.avatar_url) if member else "",
+            },
+            "title": title,
+            "description": description,
+            "color": color.value,
+            "type": "rich",
+        }
+
+    def to_dict(self) -> dict:
+        return self.embed
 
 
 class CommandEmbed(Embed):
