5620de4ade434c9f4d8bacc5066ebfa866c15b8a
citharus <citharus@protonmail.com>
2021-05-20 21:48:47 +0200

    :lipstick: Changed descriptive emote

--- Cogs/ban.py
+++ Cogs/ban.py
@@ -22,5 +22,5 @@ class Ban(commands.Cog):
         for user in banned_user:
             if (user.user.name, user.user.discriminator) == (*member.split("#"),):
                 await user.unban()
-                await ctx.send(embed=CommandEmbed(":clap: Unbanned", user))
+                await ctx.send(embed=CommandEmbed(":wave: Unbanned", user))
                 break
