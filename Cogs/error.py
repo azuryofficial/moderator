bbf6d03c795d7404228541e5ff8d271680a970f4
citharus <citharus@protonmail.com>
2021-05-25 22:23:42 +0200

    :loud_sound: Added logging for command errors

--- Cogs/error.py
+++ Cogs/error.py
@@ -1,3 +1,5 @@
+import logging
+
 import discord
 from discord.ext import commands
 
@@ -8,13 +10,16 @@ class Error(commands.Cog):
     @commands.Cog.listener()
     async def on_command_error(self, ctx: commands.Context, error) -> None:
         if isinstance(error, commands.CommandNotFound):
+            logging.warning(f"CommandNotFound: {error.args[0]}")
             await ctx.send(embed=ErrorEmbed("This command does not exist."))
 
         elif isinstance(error, commands.MissingRequiredArgument):
+            logging.error(f"MissingRequiredArgument: {error.args[0]}")
             if "member" in error.args[0]:
                 await ctx.send(embed=ErrorEmbed("You have to specify a member."))
 
         elif isinstance(error, commands.RoleNotFound):
+            logging.error(f"RequiredRoleNotFound: {error.args[0]}")
             await ctx.send(embed=ErrorEmbed("Missing role was automatically created. Try again."))
             if "Muted" in error.args[0]:
                 await ctx.guild.create_role(name="Muted",
