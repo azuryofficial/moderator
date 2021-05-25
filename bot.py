ac7e95e2c8477f980f8f877536f018656fa414b7
citharus <citharus@protonmail.com>
2021-05-25 22:25:37 +0200

    :loud_sound: Added logging config

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import logging
+
 from discord.ext import commands
 from dotenv import dotenv_values
 
@@ -12,4 +14,7 @@ bot.add_cog(Mute(bot))
 bot.add_cog(Error())
 
 if __name__ == "__main__":
+    logging.basicConfig(filename="log.log", format="[%(asctime)s]:%(levelname)s:%(message)s",
+                        datefmt="%H:%M:%S %d.%m.%Y", level=logging.WARNING)
+
     bot.run(dotenv_values(".env")["TOKEN"])
