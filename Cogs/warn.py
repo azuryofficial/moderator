2bd9b55b5a4cff5c1345c43ffa4a8b89aa11a746
citharus <citharus@protonmail.com>
2021-05-26 17:26:12 +0200

    :lipstick: Added embed to command

--- Cogs/warn.py
+++ Cogs/warn.py
@@ -1,6 +1,8 @@
 import discord
 from discord.ext import commands
 
+from embeds import CommandEmbed
+
 
 class Warn(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
@@ -9,4 +11,4 @@ class Warn(commands.Cog):
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def warn(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
-        pass
+        await ctx.send(embed=CommandEmbed(":warning: Muted", member))
