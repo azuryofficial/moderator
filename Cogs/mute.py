2b519ddd5f14f93372934ffe2ae5a2865dd314b4
citharus <citharus@protonmail.com>
2021-05-26 17:26:59 +0200

    :lipstick: Changed descriptive  emote

--- Cogs/mute.py
+++ Cogs/mute.py
@@ -14,7 +14,7 @@ class Mute(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def mute(self, ctx: commands.Context, member: discord.Member, delay: int = 1, *, reason: str = None) -> None:
         role: discord.Role = await commands.RoleConverter().convert(ctx, "Muted")
-        await ctx.send(embed=CommandEmbed(":warning: Muted", member))
+        await ctx.send(embed=CommandEmbed(":mute: Muted", member))
         await member.add_roles(role, reason=reason)
         await asyncio.sleep(delay * 60)
         await member.remove_roles(role)
