d0608489a6fe4a41df67d021406139e6da3aedc1
citharus <citharus@protonmail.com>
2021-05-29 21:42:14 +0200

    :fire: Removed add_user method

--- database.py
+++ database.py
@@ -15,16 +15,3 @@ async def setup(db: motor.AsyncIOMotorDatabase) -> None:
         except pymongo.CollectionInvalid:
             logging.error(f"Collection already exists: {collection}")
             continue
-
-
-async def add_user(db: motor.AsyncIOMotorDatabase, member: discord.Member) -> None:
-    document: dict = {
-        "_id": member.id,
-        "joined": member.joined_at,
-        "messages": 0,
-        "bans": 0,
-        "kicks": 0,
-        "mutes": 0,
-        "warns": 0,
-    }
-    await db["users"].insert_one(document)
