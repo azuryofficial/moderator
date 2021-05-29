da5caa0caec13222a400d19de58a9078478a8d83
citharus <citharus@protonmail.com>
2021-05-29 19:45:55 +0200

    :sparkles: added add_user function

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import logging
 
+import discord
 import motor.motor_asyncio as motor
 import pymongo.errors as pymongo
 from dotenv import dotenv_values
@@ -14,3 +15,16 @@ async def setup(db: motor.AsyncIOMotorDatabase) -> None:
         except pymongo.CollectionInvalid:
             logging.error(f"Collection already exists: {collection}")
             continue
+
+
+async def add_user(db: motor.AsyncIOMotorDatabase, member: discord.Member) -> None:
+    document: dict = {
+        "_id": member.id,
+        "joined": member.joined_at,
+        "messages": 0,
+        "bans": 0,
+        "kicks": 0,
+        "mutes": 0,
+        "warns": 0,
+    }
+    await db["users"].insert_one(document)
