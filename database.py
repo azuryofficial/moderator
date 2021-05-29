aaab9774b5cd948da36ae31a560854f411e3898b
citharus <citharus@protonmail.com>
2021-05-29 14:07:26 +0200

    :sparkles: Added setup function

--- database.py
+++ database.py
@@ -1,4 +1,13 @@
 import motor.motor_asyncio as motor
+import pymongo.errors as pymongo
 from dotenv import dotenv_values
 
 CLIENT: motor.AsyncIOMotorClient = motor.AsyncIOMotorClient(dotenv_values(".env")["DB"])
+
+
+async def setup(db: motor.AsyncIOMotorDatabase, collections: list[str]) -> None:
+    for collection in collections:
+        try:
+            await db.create_collection(collection)
+        except pymongo.CollectionInvalid:
+            continue
