e6e71ba30dcc52577ca1c91ff9a4913ce671be2a
citharus <citharus@protonmail.com>
2021-05-30 13:12:29 +0200

    :sparkles: added add_entry database function

--- database.py
+++ database.py
@@ -1,8 +1,12 @@
 import logging
 
+import discord
 import motor.motor_asyncio as motor
 import pymongo.errors as pymongo
+from discord.ext import commands
 from dotenv import dotenv_values
+
+from Cogs.user import User
 
 CLIENT: motor.AsyncIOMotorClient = motor.AsyncIOMotorClient(dotenv_values(".env")["DB"])
 
@@ -14,3 +18,17 @@ async def setup(db: motor.AsyncIOMotorDatabase) -> None:
         except pymongo.CollectionInvalid:
             logging.error(f"Collection already exists: {collection}")
             continue
+
+
+async def add_entry(db: motor.AsyncIOMotorDatabase, collection: str, ctx: commands.Context, member: discord.Member,
+                    reason: str) -> None:
+    await User.add_user(db, member)
+    await User.add_user(db, ctx.message.author.id)
+
+    document: dict = {
+        "member_id": member.id,
+        "reason": reason,
+        "reporter": ctx.message.author.id,
+        "timestamp": ctx.message.created_at,
+    }
+    db[collection].insert_one(document)
