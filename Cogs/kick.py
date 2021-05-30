e7022c6e4d70152505607c1547343c23bc87156e
citharus <citharus@protonmail.com>
2021-05-30 15:15:30 +0200

    :sparkles: added db entry creation for kick command

--- Cogs/kick.py
+++ Cogs/kick.py
@@ -2,6 +2,7 @@ import discord
 import motor.motor_asyncio as motor
 from discord.ext import commands
 
+from misc import add_entry
 from misc.embeds import CommandEmbed
 
 
@@ -15,3 +16,4 @@ class Kick(commands.Cog):
     async def kick(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
         await member.kick(reason=reason)
         await ctx.send(embed=CommandEmbed(":door: Kicked", member))
+        await add_entry(self.db, "kicks", ctx, member, reason)
