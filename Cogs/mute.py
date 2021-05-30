30572b3d57d3c87a91343ab272a2491bd565f191
citharus <citharus@protonmail.com>
2021-05-30 15:15:50 +0200

    :sparkles: added db entry creation for mute command

--- Cogs/mute.py
+++ Cogs/mute.py
@@ -4,6 +4,7 @@ import discord
 import motor.motor_asyncio as motor
 from discord.ext import commands
 
+from misc import add_entry
 from misc.embeds import CommandEmbed
 
 
@@ -18,5 +19,6 @@ class Mute(commands.Cog):
         role: discord.Role = await commands.RoleConverter().convert(ctx, "Muted")
         await ctx.send(embed=CommandEmbed(":mute: Muted", member))
         await member.add_roles(role, reason=reason)
+        await add_entry(self.db, "mutes", ctx, member, reason)
         await asyncio.sleep(delay * 60)
         await member.remove_roles(role)
