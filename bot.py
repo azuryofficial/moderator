4105650a0bdb7230b561673eb1720050731e0e5d
citharus <citharus@protonmail.com>
2021-05-30 15:10:32 +0200

    :adhesive_bandage: added missing db to Cogs

--- bot.py
+++ bot.py
@@ -3,15 +3,17 @@ import logging
 from discord.ext import commands
 from dotenv import dotenv_values
 
+from misc.database import CLIENT
+
 from Cogs import Kick, Ban, Mute, Error, Warn
 
 bot: commands.Bot = commands.Bot("m.")
 bot.remove_command("help")
 
-bot.add_cog(Kick(bot))
-bot.add_cog(Ban(bot))
-bot.add_cog(Mute(bot))
-bot.add_cog(Warn(bot))
+bot.add_cog(Kick(bot, CLIENT.test))
+bot.add_cog(Ban(bot, CLIENT.test))
+bot.add_cog(Mute(bot, CLIENT.test))
+bot.add_cog(Warn(bot, CLIENT.test))
 bot.add_cog(Error())
 
 if __name__ == "__main__":
