0b03d7a85279e5a806b08ceee8b5ebed5e2f2b10
citharus <citharus@protonmail.com>
2021-05-30 12:46:35 +0200

    :recycle: reworked add_user to use user_exists

--- Cogs/user.py
+++ Cogs/user.py
@@ -7,9 +7,8 @@ class User(commands.Cog):
     def __init__(self, bot: commands.Bot) -> None:
         self.bot: commands.Bot = bot
 
-    @staticmethod
-    async def _add_user(db: motor.AsyncIOMotorDatabase, member: discord.Member) -> None:
-        if not await db["users"].find_one({"_id": member.id}):
+    async def add_user(self, db: motor.AsyncIOMotorDatabase, member: discord.Member) -> None:
+        if not await self.user_exists(db, member):
             user_document: dict = {"_id": member.id}
             await db["users"].insert_one(user_document)
 
