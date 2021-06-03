c21e9dde027788cd52f6e6bc8ecbc971f65b97e0
citharus <citharus@protonmail.com>
2021-06-03 15:21:59 +0200

    :sparkles: added setup Cog

---
+++ Cogs/setup.py
@@ -0,0 +1,14 @@
+import motor.motor_asyncio as motor
+from discord.ext import commands
+
+from misc import setup
+
+
+class Setup(commands.Cog):
+    def __init__(self, bot: commands.Bot, db: motor.AsyncIOMotorDatabase) -> None:
+        self.bot: commands.Bot = bot
+        self.db: motor.AsyncIOMotorDatabase = db
+
+    @commands.Cog.listener()
+    async def on_connect(self) -> None:
+        await setup(self.db)
