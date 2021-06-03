0f7c891524943b77f9bc5dc3c33655bd386543c9
citharus <citharus@protonmail.com>
2021-06-03 17:07:54 +0200

    :sparkles: added message scan and auto warn censored word was found

--- Cogs/censor.py
+++ Cogs/censor.py
@@ -1,7 +1,10 @@
+import collections
+import discord
 import motor.motor_asyncio as motor
 from discord.ext import commands
 
-from misc import ErrorEmbed, add_word, delete_word
+from misc import ErrorEmbed, CommandEmbed, add_word, delete_word
+from Cogs.warn import _warn
 
 
 class Censor(commands.Cog):
@@ -20,3 +23,12 @@ class Censor(commands.Cog):
     async def permit(self, ctx: commands.Context, *, word: str) -> None:
         if not await delete_word(self.db, word):
             await ctx.send(embed=ErrorEmbed("The word is not censored."))
+
+    @commands.Cog.listener()
+    async def on_message(self, message: discord.Message) -> None:
+        censored_words: collections.AsyncIterable = self.db["censored"].find()
+        async for word in censored_words:
+            if word["_id"] in message.content.lower():
+                await _warn(self.db, collections.namedtuple("Author", "id")(self.bot.user.id), message.author,
+                            "Used censored words.")
+                await message.reply(embed=CommandEmbed(":warning: Warned", message.author))
