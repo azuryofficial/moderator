36c67367e2d19c7230f562d6b1312dee210c36c8
citharus <citharus@protonmail.com>
2021-06-03 15:41:36 +0200

    :sparkles: added censor Cog

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import logging
 from discord.ext import commands
 from dotenv import dotenv_values
 
-from Cogs import Kick, Ban, Mute, Error, Warn, User, Setup
+from Cogs import Kick, Ban, Mute, Error, Warn, User, Setup, Censor
 from misc.database import CLIENT
 
 bot: commands.Bot = commands.Bot("m.")
@@ -15,6 +15,7 @@ bot.add_cog(Mute(bot, CLIENT.test))
 bot.add_cog(Warn(bot, CLIENT.test))
 bot.add_cog(User(bot, CLIENT.test))
 bot.add_cog(Setup(bot, CLIENT.test))
+bot.add_cog(Censor(bot, CLIENT.test))
 bot.add_cog(Error())
 
 if __name__ == "__main__":
