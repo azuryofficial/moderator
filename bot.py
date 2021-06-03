17df1ac519d36bd1119850cb3bc1ac88e4e88c30
citharus <citharus@protonmail.com>
2021-06-03 15:49:46 +0200

    :recycle: changed to user add_cogs util

--- bot.py
+++ bot.py
@@ -4,18 +4,12 @@ from discord.ext import commands
 from dotenv import dotenv_values
 
 from Cogs import Kick, Ban, Mute, Error, Warn, User, Setup, Censor
-from misc.database import CLIENT
+from misc import CLIENT, add_cogs
 
 bot: commands.Bot = commands.Bot("m.")
 bot.remove_command("help")
 
-bot.add_cog(Kick(bot, CLIENT.test))
-bot.add_cog(Ban(bot, CLIENT.test))
-bot.add_cog(Mute(bot, CLIENT.test))
-bot.add_cog(Warn(bot, CLIENT.test))
-bot.add_cog(User(bot, CLIENT.test))
-bot.add_cog(Setup(bot, CLIENT.test))
-bot.add_cog(Censor(bot, CLIENT.test))
+add_cogs(CLIENT.test, bot, [Kick, Ban, Mute, Warn, User, Setup, Censor])
 bot.add_cog(Error())
 
 if __name__ == "__main__":
