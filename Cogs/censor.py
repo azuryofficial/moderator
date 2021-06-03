24298caee5d455377c4fd99d0f6b36498f3278cf
citharus <citharus@protonmail.com>
2021-06-03 15:29:13 +0200

    :sparkles: added censor command

--- Cogs/censor.py
+++ Cogs/censor.py
@@ -1,8 +1,16 @@
 import motor.motor_asyncio as motor
 from discord.ext import commands
 
+from misc import ErrorEmbed, add_word
+
 
 class Censor(commands.Cog):
     def __init__(self, bot: commands.Bot, db: motor.AsyncIOMotorDatabase) -> None:
         self.bot: commands.Bot = bot
         self.db: motor.AsyncIOMotorDatabase = db
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def censor(self, ctx: commands.Context, *, word: str) -> None:
+        if not await add_word(self.db, word):
+            await ctx.send(embed=ErrorEmbed("Word is already censored."))
