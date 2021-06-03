c2010d2824f4419eae80d4bb9c589563415caad1
citharus <citharus@protonmail.com>
2021-06-03 15:35:40 +0200

    :lipstick: changed error embed message

--- Cogs/censor.py
+++ Cogs/censor.py
@@ -13,7 +13,7 @@ class Censor(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def censor(self, ctx: commands.Context, *, word: str) -> None:
         if not await add_word(self.db, word):
-            await ctx.send(embed=ErrorEmbed("Word is already censored."))
+            await ctx.send(embed=ErrorEmbed("The word is already censored."))
 
     @commands.command()
     @commands.has_permissions(kick_members=True)
