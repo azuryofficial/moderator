0e9d598e7323baee91fd910dcd19fbd080e676d9
citharus <citharus@protonmail.com>
2021-06-03 16:35:29 +0200

    :art: moved _warn method out of Cog and renamed it

--- Cogs/warn.py
+++ Cogs/warn.py
@@ -6,6 +6,11 @@ from misc import add_entry
 from misc.embeds import CommandEmbed
 
 
+async def warn(db: motor.AsyncIOMotorDatabase, author: discord.Member, member: discord.Member,
+               reason: str) -> None:
+    await add_entry(db, "warns", author, member, reason)
+
+
 class Warn(commands.Cog):
     def __init__(self, bot: commands.Bot, db: motor.AsyncIOMotorDatabase) -> None:
         self.bot: commands.Bot = bot
@@ -15,9 +20,4 @@ class Warn(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def warn(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
         await ctx.send(embed=CommandEmbed(":warning: Warned", member))
-        await self._warn(self.db, ctx.message.author, member, reason)
-
-    @staticmethod
-    async def _warn(db: motor.AsyncIOMotorDatabase, author: discord.Member, member: discord.Member,
-                    reason: str) -> None:
-        await add_entry(db, "warns", author, member, reason)
+        await warn(self.db, ctx.message.author, member, reason)
