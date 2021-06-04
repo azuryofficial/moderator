13826d8f695015f32c1650af98f88440f1d3d748
citharus <citharus@protonmail.com>
2021-06-04 21:18:51 +0200

    :sparkles: added time parameter to ban command

--- Cogs/ban.py
+++ Cogs/ban.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import discord
 import motor.motor_asyncio as motor
 from discord.ext import commands
@@ -15,10 +17,12 @@ class Ban(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
-    async def ban(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
+    async def ban(self, ctx: commands.Context, member: discord.Member, time: int, *, reason: str = None) -> None:
         await member.ban(reason=reason)
         await ctx.send(embed=CommandEmbed(":no_entry: Banned", member))
         await add_entry(self.db, "bans", ctx.message.author, member, reason)
+        await asyncio.sleep(86400 * time)
+        await member.unban()
 
     @commands.command()
     @commands.has_permissions(ban_members=True)
