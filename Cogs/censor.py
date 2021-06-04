8560ed3ac27434c33ab915db70771c1b7796628a
citharus <citharus@protonmail.com>
2021-06-04 23:02:45 +0200

    :recycle: update censor listener to use new config

--- Cogs/censor.py
+++ Cogs/censor.py
@@ -4,8 +4,9 @@ import discord
 import motor.motor_asyncio as motor
 from discord.ext import commands
 
-from Cogs.warn import _warn
-from misc import ErrorEmbed, CommandEmbed, add_word, delete_word
+from Cogs import _warn
+from misc import ErrorEmbed, CommandEmbed, add_word, delete_word, replace_placeholders
+from misc.config import COMMANDS
 
 __all__: list[str] = ["Censor"]
 
@@ -29,9 +30,13 @@ class Censor(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message) -> None:
-        censored_words: collections.AsyncIterable = self.db["censored"].find()
+        censored_words: collections.AsyncIterable = self.db[COMMANDS["CENSOR"].collection].find()
         async for word in censored_words:
             if word["_id"] in message.content.lower():
                 await _warn(self.db, collections.namedtuple("Author", "id")(self.bot.user.id), message.author,
-                            "Used censored words.")
-                await message.reply(embed=CommandEmbed(":warning: Warned", message.author))
+                            COMMANDS["CENSOR"].reason)
+                replacement: dict = {"{member}": message.author.mention, "{reason}": COMMANDS["CENSOR"].reason}
+                await message.reply(embed=CommandEmbed(replace_placeholders(COMMANDS["CENSOR"].title, replacement),
+                                                       replace_placeholders(COMMANDS["CENSOR"].description,
+                                                                            replacement),
+                                                       message.author))
