fc92f6be93d18b8c4646ce8f82b83b7ea7b5e753
citharus <citharus@protonmail.com>
2021-06-04 21:51:21 +0200

    :sparkles: added replace_placeholders util

--- misc/utils.py
+++ misc/utils.py
@@ -3,9 +3,15 @@ from typing import Type
 import motor.motor_asyncio as motor
 from discord.ext import commands
 
-__all__: list[str] = ["add_cogs"]
+__all__: list[str] = ["add_cogs", "replace_placeholders"]
 
 
 def add_cogs(db: motor.AsyncIOMotorDatabase, bot: commands.Bot, cogs: list[Type[commands.Cog]]) -> None:
     for cog in cogs:
         bot.add_cog(cog(bot, db))
+
+
+def replace_placeholders(string: str, replacements: dict) -> str:
+    for original, replacement in replacements.items():
+        string = string.replace(original, replacement)
+    return string
