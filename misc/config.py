047b6b350248349b25c163e94dd0044b74e0142e
citharus <citharus@protonmail.com>
2021-06-04 18:38:59 +0200

    :sparkles: added default config paths

--- misc/config.py
+++ misc/config.py
@@ -1,4 +1,12 @@
 import configparser as config
+import pathlib
+from typing import Dict
+
+PATHS: Dict[str, pathlib.Path] = {
+    "project": pathlib.Path("./config.ini"),
+    "home": pathlib.Path.home().joinpath(pathlib.Path(".AzuryModerator.ini")),
+    "config": pathlib.Path.home().joinpath(pathlib.Path(".config/AzuryModerator/config.ini")),
+}
 
 CONFIG: config.ConfigParser = config.ConfigParser()
-CONFIG.read("./config")
+CONFIG.read("./config.ini")
