8fe22e3ba54494fb9a50b4cd3d0eb16e241f6042
citharus <citharus@protonmail.com>
2021-06-04 21:01:54 +0200

    :recycle: changed import format

--- misc/__init__.py
+++ misc/__init__.py
@@ -1,3 +1,3 @@
-from .database import add_entry, add_user, add_word, delete_word, setup, CLIENT
-from .embeds import CommandEmbed, ErrorEmbed
-from .utils import add_cogs
+from .database import *
+from .embeds import *
+from .utils import *
