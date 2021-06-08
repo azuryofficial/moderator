99a6c8cfc6e235eed04270a36e66c8fc6af7c445
citharus <citharus@protonmail.com>
2021-06-08 21:21:59 +0200

    :sparkles: added user info command

--- Cogs/user.py
+++ Cogs/user.py
@@ -1,5 +1,12 @@
+from collections import defaultdict
+from datetime import datetime
+
+import discord
 import motor.motor_asyncio as motor
 from discord.ext import commands
+
+from misc import UserEmbed
+from misc.config import COMMANDS
 
 __all__: list[str] = ["User"]
 
@@ -8,3 +15,17 @@ class User(commands.Cog):
     def __init__(self, bot: commands.Bot, db: motor.AsyncIOMotorDatabase) -> None:
         self.bot: commands.Bot = bot
         self.db: motor.AsyncIOMotorDatabase = db
+
+    @commands.command()
+    async def user_info(self, ctx: commands.Context, member: discord.Member = None) -> None:
+        if not member:
+            member: discord.Member = ctx.author
+
+        info: defaultdict = defaultdict(str)
+        for amount in ["BAN", "KICK", "WARN", "MUTE"]:
+            info[f"{amount.lower()}s"] = str(
+                await self.db[COMMANDS[amount].collection].count_documents({"member_id": member.id}),
+            )
+
+        joined: datetime = datetime.strftime(member.joined_at, COMMANDS["USER"].format)
+        await ctx.send(embed=UserEmbed(member, **info, joined=joined))
