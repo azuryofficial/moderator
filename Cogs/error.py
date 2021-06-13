f1cfedbcadf581f47f46e0776b5e2d7c58330ef4
citharus <citharus@protonmail.com>
2021-06-13 16:00:53 +0200

    :art: reformatted long statements

--- Cogs/error.py
+++ Cogs/error.py
@@ -14,26 +14,32 @@ class Error(commands.Cog):
     async def on_command_error(self, ctx: commands.Context, error) -> None:
         if isinstance(error, commands.CommandNotFound):
             replacement: dict = {"{command}": error.args[0].split()[1]}
+
             logging.error(replace_placeholders(ERRORS["CNF"].log, replacement))
             await ctx.send(embed=ErrorEmbed(replace_placeholders(ERRORS["CNF"].embed, replacement)))
 
         elif isinstance(error, commands.MissingRequiredArgument):
             replacement: dict = {"{command}": str(ctx.command), "{argument}": error.args[0].split()[0]}
+
             logging.warning(replace_placeholders(ERRORS["MRA"].log, replacement))
             await ctx.send(embed=ErrorEmbed(replace_placeholders(ERRORS["MRA"].embed, replacement)))
 
         elif isinstance(error, commands.RoleNotFound):
             replacement: dict = {"{role}": error.args[0].split()[1]}
+
             logging.error(replace_placeholders(ERRORS["RNF"].log, replacement))
             await ctx.send(embed=ErrorEmbed(replace_placeholders(ERRORS["RNF"].embed, replacement)))
+
             if "Muted" in error.args[0]:
-                await ctx.guild.create_role(name="Muted",
-                                            permissions=discord.Permissions(read_message_history=True,
-                                                                            read_messages=True),
-                                            reason="Automatically created role for mute command.")
+                await ctx.guild.create_role(
+                    name="Muted",
+                    permissions=discord.Permissions(read_message_history=True, read_messages=True),
+                    reason="Automatically created role for mute command.",
+                )
 
         elif isinstance(error, commands.MemberNotFound):
             replacement: dict = {"{member}": error.args[0]}
+
             logging.warning(replace_placeholders(ERRORS["MNF"].log, replacement))
             await ctx.send(embed=ErrorEmbed(replace_placeholders(ERRORS["MNF"].embed, replacement)))
 
