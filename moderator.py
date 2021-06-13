3d9041109e8bf2e5df7e1c0ee34966ba9464e6fd
citharus <citharus@protonmail.com>
2021-06-13 16:08:56 +0200

    :art: optimized imports

--- moderator.py
+++ moderator.py
@@ -4,16 +4,27 @@ from discord.ext import commands
 from dotenv import dotenv_values
 
 from Cogs import *
-from misc import CLIENT, add_cogs
+from misc import (
+    CLIENT,
+    add_cogs,
+)
 
 bot: commands.Bot = commands.Bot("m.")
 bot.remove_command("help")
 
-add_cogs(CLIENT.test, bot, [Kick, Ban, Mute, Warn, User, Setup, Censor])
+add_cogs(
+    CLIENT.test,
+    bot,
+    [Kick, Ban, Mute, Warn, User, Setup, Censor],
+)
 bot.add_cog(Error())
 
 if __name__ == "__main__":
-    logging.basicConfig(filename="log.log", format="[%(asctime)s]:[%(levelname)s]:%(message)s",
-                        datefmt="%H:%M:%S %d.%m.%Y", level=logging.WARNING)
+    logging.basicConfig(
+        filename="log.log",
+        format="[%(asctime)s]:[%(levelname)s]:%(message)s",
+        datefmt="%H:%M:%S %d.%m.%Y",
+        level=logging.WARNING,
+    )
 
     bot.run(dotenv_values(".env")["TOKEN"])
