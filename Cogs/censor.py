1f422378523a992f0fe35c905b9d53b7b00a2138
citharus <citharus@protonmail.com>
2021-06-13 15:51:54 +0200

    :art: reformatted long statements

--- Cogs/censor.py
+++ Cogs/censor.py
@@ -5,7 +5,13 @@ import motor.motor_asyncio as motor
 from discord.ext import commands
 
 from Cogs.warn import _warn
-from misc import ErrorEmbed, CommandEmbed, add_word, delete_word, replace_placeholders
+from misc import (
+    ErrorEmbed,
+    CommandEmbed,
+    add_word,
+    delete_word,
+    replace_placeholders,
+)
 from misc.config import COMMANDS
 
 __all__: list[str] = ["Censor"]
@@ -32,13 +38,20 @@ class Censor(commands.Cog):
     async def on_message(self, message: discord.Message) -> None:
         if message.content.startswith("m."):
             return None
+
         censored_words: collections.AsyncIterable = self.db[COMMANDS["CENSOR"].collection].find()
         async for word in censored_words:
             if word["_id"] in message.content.lower():
-                await _warn(self.db, collections.namedtuple("Author", "id")(self.bot.user.id), message.author,
-                            COMMANDS["CENSOR"].reason)
+                await _warn(
+                    self.db,
+                    collections.namedtuple("Author", "id")(self.bot.user.id),
+                    message.author,
+                    COMMANDS["CENSOR"].reason,
+                )
+
                 replacement: dict = {"{member}": message.author.mention, "{reason}": COMMANDS["CENSOR"].reason}
-                await message.reply(embed=CommandEmbed(replace_placeholders(COMMANDS["CENSOR"].title, replacement),
-                                                       replace_placeholders(COMMANDS["CENSOR"].description,
-                                                                            replacement),
-                                                       message.author))
+                await message.reply(embed=CommandEmbed(
+                    replace_placeholders(COMMANDS["CENSOR"].title, replacement),
+                    replace_placeholders(COMMANDS["CENSOR"].description, replacement),
+                    message.author,
+                ))
