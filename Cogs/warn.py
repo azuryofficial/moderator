7eaeae2b73aa80d2ee0ab742f0b1968859e124e7
citharus <citharus@protonmail.com>
2021-06-13 16:03:11 +0200

    :art: reformatted file

--- Cogs/warn.py
+++ Cogs/warn.py
@@ -6,7 +6,11 @@ import motor.motor_asyncio as motor
 from discord.ext import commands
 
 from Cogs.kick import _kick
-from misc import CommandEmbed, add_entry, replace_placeholders
+from misc import (
+    CommandEmbed,
+    add_entry,
+    replace_placeholders,
+)
 from misc.config import COMMANDS
 
 __all__: list[str] = ["Warn", "_warn"]
@@ -16,6 +20,7 @@ async def _warn(db: motor.AsyncIOMotorDatabase, author: Union[discord.Member, co
                 member: discord.Member, reason: str) -> None:
     if await db[COMMANDS["WARN"].collection].count_documents({"member_id": member.id}) >= COMMANDS["WARN"].threshold:
         await _kick(db, author, member, reason)
+
     await add_entry(db, COMMANDS["WARN"].collection, author, member, reason)
 
 
@@ -27,8 +32,11 @@ class Warn(commands.Cog):
     @commands.command(aliases=["w"])
     @commands.has_permissions(kick_members=True)
     async def warn(self, ctx: commands.Context, member: discord.Member, *, reason: str = None) -> None:
-        replacement: dict = {"{member}": member.mention, "{reason}": reason or ""}
-        await ctx.send(embed=CommandEmbed(replace_placeholders(COMMANDS["WARN"].title, replacement),
-                                          replace_placeholders(COMMANDS["WARN"].description, replacement),
-                                          member))
         await _warn(self.db, ctx.message.author, member, reason)
+
+        replacement: dict = {"{member}": member.mention, "{reason}": reason or ""}
+        await ctx.send(embed=CommandEmbed(
+            replace_placeholders(COMMANDS["WARN"].title, replacement),
+            replace_placeholders(COMMANDS["WARN"].description, replacement),
+            member,
+        ))
