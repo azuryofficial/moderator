4d655e509e728242fe53d0c465126ce2aa5379df
citharus <citharus@protonmail.com>
2021-06-13 15:56:07 +0200

    :art: reformatted long statements

--- Cogs/mute.py
+++ Cogs/mute.py
@@ -4,7 +4,11 @@ import discord
 import motor.motor_asyncio as motor
 from discord.ext import commands
 
-from misc import CommandEmbed, add_entry, replace_placeholders
+from misc import (
+    CommandEmbed,
+    add_entry,
+    replace_placeholders,
+)
 from misc.config import COMMANDS
 
 __all__: list[str] = ["Mute"]
@@ -19,11 +23,15 @@ class Mute(commands.Cog):
     @commands.has_permissions(kick_members=True)
     async def mute(self, ctx: commands.Context, member: discord.Member, delay: int = 1, *, reason: str = None) -> None:
         role: discord.Role = await commands.RoleConverter().convert(ctx, "Muted")
-        replacement: dict = {"{member}": member.mention, "{time}": delay, "{reason}": reason or ""}
-        await ctx.send(embed=CommandEmbed(replace_placeholders(COMMANDS["MUTE"].title, replacement),
-                                          replace_placeholders(COMMANDS["MUTE"].description, replacement),
-                                          member))
         await member.add_roles(role, reason=reason)
         await add_entry(self.db, COMMANDS["MUTE"].collection, ctx.message.author, member, reason)
+
+        replacement: dict = {"{member}": member.mention, "{time}": delay, "{reason}": reason or ""}
+        await ctx.send(embed=CommandEmbed(
+            replace_placeholders(COMMANDS["MUTE"].title, replacement),
+            replace_placeholders(COMMANDS["MUTE"].description, replacement),
+            member,
+        ))
+
         await asyncio.sleep(delay * 60)
         await member.remove_roles(role)
