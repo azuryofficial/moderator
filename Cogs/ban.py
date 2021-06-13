5e0f59d54974c82c52f6588a6ecf38e4dd169371
citharus <citharus@protonmail.com>
2021-06-13 15:59:18 +0200

    :art: reformatted long statements

--- Cogs/ban.py
+++ Cogs/ban.py
@@ -4,7 +4,11 @@ import discord
 import motor.motor_asyncio as motor
 from discord.ext import commands
 
-from misc import CommandEmbed, add_entry, replace_placeholders
+from misc import (
+    CommandEmbed,
+    add_entry,
+    replace_placeholders,
+)
 from misc.config import COMMANDS
 
 __all__: list[str] = ["Ban"]
@@ -19,11 +23,15 @@ class Ban(commands.Cog):
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx: commands.Context, member: discord.Member, time: int, *, reason: str = None) -> None:
         await member.ban(reason=reason)
-        replacement: dict = {"{member}": member.mention, "{reason}": reason or "", "{time}": str(time)}
-        await ctx.send(embed=CommandEmbed(replace_placeholders(COMMANDS["BAN"].title, replacement),
-                                          replace_placeholders(COMMANDS["BAN"].description, replacement),
-                                          member))
         await add_entry(self.db, COMMANDS["BAN"].collection, ctx.message.author, member, reason)
+
+        replacement: dict = {"{member}": member.mention, "{reason}": reason or "", "{time}": str(time)}
+        await ctx.send(embed=CommandEmbed(
+            replace_placeholders(COMMANDS["BAN"].title, replacement),
+            replace_placeholders(COMMANDS["BAN"].description, replacement),
+            member,
+        ))
+
         await asyncio.sleep(86400 * time)
         await member.unban()
 
@@ -31,12 +39,15 @@ class Ban(commands.Cog):
     @commands.has_permissions(ban_members=True)
     async def unban(self, ctx: commands.Context, *, member: str) -> None:
         banned_user: list[discord.Member] = await ctx.guild.bans()
-
         for user in banned_user:
             if (user.user.name, user.user.discriminator) == (*member.split("#"),):
                 await user.unban()
+
                 replacement: dict = {"{member}": user.mention}
-                await ctx.send(embed=CommandEmbed(replace_placeholders(COMMANDS["UNBAN"].title, replacement),
-                                                  replace_placeholders(COMMANDS["UNBAN"].description, replacement),
-                                                  user))
+                await ctx.send(embed=CommandEmbed(
+                    replace_placeholders(COMMANDS["UNBAN"].title, replacement),
+                    replace_placeholders(COMMANDS["UNBAN"].description, replacement),
+                    user,
+                ))
+
                 break
